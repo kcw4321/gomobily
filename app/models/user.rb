6341a3552b8632d2fd1a5f66526522d3a30b9577
rougeaux marie <mary.rougeaux@gmail.com>
2015-07-02 15:45:28 +0200

    facebook connect

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 #
 # Table name: users
 #
+
 #  id                     :integer          not null, primary key
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
@@ -20,6 +21,12 @@
 #  picture_file_size      :integer
 #  picture_updated_at     :datetime
 #  name                   :string
+#  provider               :string
+#  uid                    :string
+#  picture                :string
+#  token                  :string
+#  token_expiry           :datetime
+
 #
 # Indexes
 #
@@ -31,7 +38,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
 
   has_many :reviews, dependent: :destroy
   has_many :locations, dependent: :destroy
@@ -40,4 +48,19 @@ class User < ActiveRecord::Base
     styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/
+end
+
+class User < ActiveRecord::Base
+  def self.find_for_facebook_oauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+     user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]  # Fake password for validation
+      user.name = auth.info.name
+      user.picture = auth.info.image
+      user.token = auth.credentials.token
+      user.token_expiry = Time.at(auth.credentials.expires_at)
+      end
+  end
 end
