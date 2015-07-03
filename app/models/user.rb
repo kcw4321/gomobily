e6fa366c69e01eb04938cdd1fa5eaf1f8fe34cd8
Katy Webster <katywebster50@yahoo.co.uk>
2015-07-03 09:41:38 +0200

    changed mobility category to dropdown

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,38 @@
 #
 # Table name: users
 #
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  picture_file_name      :string
+#  picture_content_type   :string
+#  picture_file_size      :integer
+#  picture_updated_at     :datetime
+#  name                   :string
+#  city                   :string
+#  country                :string
+#  mobility_category      :string
+#  provider               :string
+#  uid                    :string
+#  picture                :string
+#  token                  :string
+#  token_expiry           :datetime
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
 
 #  id                     :integer          not null, primary key
 #  email                  :string           default(""), not null
